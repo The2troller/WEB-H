80c1e2f59363cb5b8a842e4d201aad2b0564f61a
robervt09 <rlarao@estudiante.uc.cl>
2025-10-19 20:36:20 -0300

    Modo oscuro

---
+++ main.js
@@ -0,0 +1,13 @@
+const boton = document.getElementById('modo-boton');
+const body = document.body;
+
+boton.addEventListener('click', () =>  {
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        boton.textContent = 'Desactivar Modo Oscuro';
+    }
+    else {
+        boton.textContent = 'Activar Modo Oscuro';
+    }
+});
---
+++ main.py
@@ -0,0 +1,9 @@
+#Hola a todos
+#Prueben que estan vivos
+#Renato --> 1
+#Franco --> 0
+#Roberto --> 1
+#Roberto el grande
+
+for i in range(3):
+    print(i)
